e4a329d58bbc69f27f659a0e1879c7d63d996d4a
jacobhak <jacobhakansson@gmail.com>
2012-12-11 09:50:00 +0100

    Initial Commit

---
+++ Shutterbug/ShutterbugViewController.m
@@ -0,0 +1,29 @@
+//
+//  ShutterbugViewController.m
+//  Shutterbug
+//
+//  Created by Jacob Håkansson on 2012-12-11.
+//  Copyright (c) 2012 Jacob Håkansson. All rights reserved.
+//
+
+#import "ShutterbugViewController.h"
+
+@interface ShutterbugViewController ()
+
+@end
+
+@implementation ShutterbugViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Shutterbug/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Shutterbug
+//
+//  Created by Jacob Håkansson on 2012-12-11.
+//  Copyright (c) 2012 Jacob Håkansson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ShutterbugAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ShutterbugAppDelegate class]));
+    }
+}
